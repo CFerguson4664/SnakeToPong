4f5d64dc1c2e7d7c205b72359b9f44b957d543e8
Harrison Hiatt <hlhiatt20@my.trine.edu>
2022-10-04 15:27:20 -0400

    Update pong_gameplay.c

--- SnakeToPong/Core/Src/pong_gameplay.c
+++ SnakeToPong/Core/Src/pong_gameplay.c
@@ -30,10 +30,10 @@ void pong_game_init(pong_game* p){
 	p->balldir.y = initial_balldir.y;
 
 	//Paddle Stuff
-	const XY_PT initial_Lpad = {0,4};
-	const XY_PT initial_Rpad = {7,4};
+	const XY_PT initial_Lpad = {0,4}; //Left Paddle values
+	const XY_PT initial_Rpad = {7,4}; //Right Paddle values
 
-	p->Lpad.x = initial_Lpad.x;
+	p->Lpad.x = initial_Lpad.x; //Places the {x,y} values into the left and right paddle objects.
 	p->Lpad.y = initial_Lpad.y;
 	p->Rpad.x = initial_Rpad.x;
 	p->Rpad.y = initial_Rpad.y;
@@ -49,11 +49,11 @@ void pong_periodic_play(pong_game* p){
 
 	//if statement to check if temp ball hit wall
 	if( tempBall.x < 0){ //if the temp ball hit left wall
-		p->balldir.x = 0;
+		p->balldir.x = 0; //Stops the ball from moving in the x and y direction.
 		p->balldir.y = 0;
 	}
 	else if(tempBall.x > 7){ //if temp ball hits the right wall
-		p->balldir.x = 0;
+		p->balldir.x = 0; //Stops the ball from moving in the x and y direction.
 		p->balldir.y = 0;
 	}
 	else if(tempBall.y < 0){ //if we hit the top wall
@@ -70,10 +70,10 @@ void pong_periodic_play(pong_game* p){
 
 	//if statement to check if the temp ball hit the paddle
 	if(tempBall2.x <= 0 && (p->Lpad.y + 1 == tempBall2.y || p->Lpad.y == tempBall2.y || p->Lpad.y-1 == tempBall2.y)){ //if temp ball hits left paddle
-		p->balldir.x *= -1;
+		p->balldir.x *= -1; //reverse the x direction of the ball
 	}
 	else if(tempBall2.x >= 7 && (p->Rpad.y + 1 == tempBall2.y || p->Rpad.y == tempBall2.y || p->Rpad.y-1 == tempBall2.y)){ //if temp ball hits right paddle
-		p->balldir.x *= -1;
+		p->balldir.x *= -1; //reverse x direction of the ball
 	}
 
 	p->ball.x += p->balldir.x; //Moves the real ball once we check to see if the move is valid.
@@ -88,7 +88,7 @@ void pacify_compiler(){
 void paddle_update(pong_game* p,Smc_queue* q){
 	Q_data msg; //Variable that will hold the input request.
 	bool data_available;
-	data_available = q->get(q,&msg);
+	data_available = q->get(q,&msg); //Test to see of there is input data
 
 	while(data_available) {
 		if(!data_available) return;
@@ -96,22 +96,22 @@ void paddle_update(pong_game* p,Smc_queue* q){
 			switch(msg.button){
 			case Left_Up:
 				if(p->Lpad.y > 1){
-					p->Lpad.y -= 1;
+					p->Lpad.y -= 1; //move left paddle down
 				}
 				break;
 			case Left_Down:
 				if(p->Lpad.y < 6){
-					p->Lpad.y += 1;
+					p->Lpad.y += 1; //move left paddle up
 				}
 				break;
 			case Right_Up:
 				if(p->Rpad.y > 1){
-					p->Rpad.y -= 1;
+					p->Rpad.y -= 1; //move right paddle up
 				}
 				break;
 			case Right_Down:
 				if(p->Rpad.y < 6){
-					p->Rpad.y += 1;
+					p->Rpad.y += 1; //Move right paddle down
 				}
 				break;
 			default: //do nothing
